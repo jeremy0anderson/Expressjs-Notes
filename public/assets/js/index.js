3d45d582cbf77a21a10332a5e4eda88a40667458
jeremy0anderson <projects@jeremyanderson.dev>
2022-02-26 07:12:13 -0700

    add heroku to git

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -180,4 +180,4 @@ if (window.location.pathname === '/notes') {
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
 
-getAndRenderNotes();
+getAndRenderNotes().then(r => console.log(noteList));
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const {fs} = require('fs-extra');
 const router = require('express').Router();
 const {notes}= require('./db/db.json');
 const port = process.env.PORT || 4000;
+
+function newNote(body, notesArray){
+      let note= body;
+      notesArray.push(note);
+      return fs.writeFileSync("./db/db.json",
+          JSON.stringify({notes: notesArray,}, null, 2));
+}
+
+
+
+
+
 app.route('/api/notes')
       .get((req, res) =>{
             res.send(notes)
       })
       .post((req, res) =>{
-      
+            res.send(newNote(req.body, notes));
       });
 
 app.route('/notes')
@@ -19,3 +32,7 @@ app.route('/notes')
       .post((req, res) =>{
       
       });
+
+app.listen(port, () =>{
+       console.log("Listening on port "+port);
+})
